a2f12be603950fb9713c0ec1d81663e5dc8fb5e7
DongChiMin <namv2704@gmail.com>
2026-04-02 10:40:58 +0700

    Fix: Laboratory

---
+++ Assets/_Project/Modules/Laboratory/Scripts/SceneLabIngredientPooling.cs
@@ -0,0 +1,8 @@
+
+namespace LabDiner.Laboratory
+{
+    public class SceneLabIngredientPooling : SceneObjectPooling<LabIngredientContext>
+    {
+        // Class này có thể dùng để khởi tạo pool nếu cần, hoặc để chứa các hàm tiện ích liên quan đến pooling trong lab
+    }
+}
